bf5d3e1e99b0919287b7bf09d76e8e72a0b448f8
humuyar <humiq6071@gmail.com>
2024-08-20 02:48:43 -0700

    added make a placeholder for makrdwon preview

---
+++ markdwon-previewer/src/placeholder.js
@@ -0,0 +1,42 @@
+const placeholder = `
+# Sample Markdown Header Level
+
+## Sample Header Level 2
+
+### Link
+
+Here's a link to [Codepen](https://codepen.io).
+
+### Code Block
+
+1.  Open the file.
+2.  Find the following code block on line 21:
+
+        <html>
+          <head>
+            <title>Test</title>
+          </head>
+
+3.  Update the title to match the name of your website.
+
+### Inline Code
+
+I think you should use an \`<addr>\` element here instead.
+
+### List
+
+- First item
+- Second item
+
+### Blockquote
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+### Image
+
+![Markdown Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/208px-Markdown-mark.svg.png "Markdown Logo")
+
+### Bold Text
+
+I just love **bold text**.
+`;
