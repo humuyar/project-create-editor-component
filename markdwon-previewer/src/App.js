b197ffd6844851d626871022689bc99cd45e5049
humuyar <humiq6071@gmail.com>
2024-08-20 00:17:01 -0700

    added a react app

---
+++ markdwon-previewer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>humi</h2>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
